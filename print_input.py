c8724a84474de913e0008e7ce2a39db57ae9816a
João Luís <165718760+JoaoLuispteles@users.noreply.github.com>
2024-04-05 17:10:11 -0300

    Delete print_input.py

--- print_input.py
+++
@@ -1,7 +0,0 @@
-nome = input("Informe o seu Nome:")
-idade = input("Informe a sua Idade:")
-
-print(nome, idade)
-print(nome, idade, end="... \n")
-print(nome, idade, sep="#", end="... \n")
-print(nome, idade, sep="#")
